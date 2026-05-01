694bac96880a186539a2534fa45b959bc62c6dba
JulienBonet <julienbonetpro@gmail.com>
2026-05-01 02:58:58 +0200

    add logo in header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
-import "./header.css"
+import { Box, Stack } from '@mui/material';
 
 const Header = () => {
   return (
-    <div className='header'>
-    </div>
+    <Stack
+      direction="row"
+      sx={{
+        height: 50,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'sticky',
+        top: 0,
+        zIndex: 1300,
+      }}
+    >
+      <Box component="img" src="/images/radio_face_b_logo.png" alt="Radio Face B" />
+    </Stack>
   );
 };
 
