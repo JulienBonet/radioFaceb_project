3865801b085c4acc1c5a994310f07919a3268aff
JulienBonet <julienbonetpro@gmail.com>
2026-06-17 15:41:19 +0200

    Fix nodemailer smtp configuration

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -3,11 +3,16 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+
+  family: 4,
 });
 
 export default transporter;
