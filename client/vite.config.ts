e14df21c1d13a72a1ed9dc47ba68e8afb9b108c7
JulienBonet <julienbonetpro@gmail.com>
2026-06-20 00:37:53 +0200

    Add PWA support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,52 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: ['images/PWA_FB_logo_main.png', 'images/PWA_FB_logo_mask.png'],
+
+      manifest: {
+        name: 'Radio Face B',
+        short_name: 'Radio Face B',
+
+        description: 'Web radio indépendante',
+
+        theme_color: '#000000',
+        background_color: '#000000',
+
+        display: 'standalone',
+        orientation: 'portrait',
+
+        start_url: '/',
+        scope: '/',
+
+        lang: 'fr',
+
+        icons: [
+          {
+            src: '/images/PWA_FB_logo_main.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+
+          {
+            src: '/images/PWA_FB_logo_mask.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 
   resolve: {
     alias: {
