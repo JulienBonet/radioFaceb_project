66dc92ca7204eb4a38e3c43090cd28a077e7bda0
JulienBonet <julienbonetpro@gmail.com>
2026-06-23 20:16:29 +0200

    console.log in trackevent

--- client/src/utils/matomo.ts
+++ client/src/utils/matomo.ts
@@ -1,10 +1,27 @@
 // src/utils/matomo.ts
 
+// export const trackEvent = (
+//   category: string,
+//   action: string,
+//   name?: string
+// ) => {
+//   if (!window._paq) return;
+
+//   window._paq.push([
+//     'trackEvent',
+//     category,
+//     action,
+//     name,
+//   ]);
+// };
+
 export const trackEvent = (
   category: string,
   action: string,
   name?: string
 ) => {
+  console.log('TRACK EVENT', category, action, name);
+
   if (!window._paq) return;
 
   window._paq.push([
